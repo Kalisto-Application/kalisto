694253bf17830bf1f7b666c4b9040a2bf36466e6
Denis <candyboobers@gmail.com>
2023-06-08 10:55:04 +0200

    Implement proto compiler

---
+++ src/internal/proto/compiler/compiler.go
@@ -0,0 +1,67 @@
+package compiler
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/jhump/protoreflect/desc"
+	"github.com/jhump/protoreflect/desc/protoparse"
+	"google.golang.org/protobuf/reflect/protodesc"
+	"google.golang.org/protobuf/reflect/protoregistry"
+
+	"google.golang.org/protobuf/types/descriptorpb"
+)
+
+var (
+	ErrNoFiles = errors.New("compiler: files are required to compile proto")
+)
+
+type FileCompiler struct {
+	seen map[string]struct{}
+}
+
+func NewFileCompiler() *FileCompiler {
+	return &FileCompiler{seen: make(map[string]struct{})}
+}
+
+func (c *FileCompiler) Compile(paths, filenames []string) (*protoregistry.Files, error) {
+	if len(filenames) == 0 {
+		return nil, ErrNoFiles
+	}
+
+	files, err := protoparse.ResolveFilenames(paths, filenames...)
+	if err != nil {
+		return nil, fmt.Errorf("compiler: failed to resolve files: %w", err)
+	}
+
+	parser := protoparse.Parser{ImportPaths: paths}
+	descriptors, err := parser.ParseFiles(files...)
+	if err != nil {
+		return nil, fmt.Errorf("compiler: failed to parse files: %w", err)
+	}
+
+	fdset := &descriptorpb.FileDescriptorSet{}
+	for _, fd := range descriptors {
+		fdset.File = append(fdset.File, c.walk(fd)...)
+	}
+
+	return protodesc.NewFiles(fdset)
+}
+
+func (c *FileCompiler) walk(fd *desc.FileDescriptor) []*descriptorpb.FileDescriptorProto {
+	descriptorsProto := []*descriptorpb.FileDescriptorProto{}
+
+	key := fd.GetName() + fd.GetPackage()
+	if _, ok := c.seen[key]; ok {
+		return descriptorsProto
+	}
+	c.seen[key] = struct{}{}
+	descriptorsProto = append(descriptorsProto, fd.AsFileDescriptorProto())
+
+	for _, dep := range fd.GetDependencies() {
+		deps := c.walk(dep)
+		descriptorsProto = append(descriptorsProto, deps...)
+	}
+
+	return descriptorsProto
+}
---
+++ src/internal/proto/compiler/compiler_test.go
@@ -0,0 +1,88 @@
+package compiler_test
+
+import (
+	"kalisto/src/internal/proto/compiler"
+	"os"
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+	"google.golang.org/protobuf/reflect/protoreflect"
+)
+
+type FileCompilerTestSuite struct {
+	suite.Suite
+
+	c  *compiler.FileCompiler
+	wd string
+}
+
+func (s *FileCompilerTestSuite) SetupTest() {
+	s.c = compiler.NewFileCompiler()
+	wd, err := os.Getwd()
+	s.Require().NoError(err)
+	s.wd = wd
+}
+
+func (s *FileCompilerTestSuite) TestSingleFileWuthNoDeps() {
+	protoPath := path.Join(s.wd, "..", "..", "..", "..", "tests/examples/proto/service.proto")
+
+	c := compiler.NewFileCompiler()
+	fileRegistry, err := c.Compile([]string{path.Dir(protoPath)}, []string{protoPath})
+	s.NoError(err)
+
+	s.EqualValues(fileRegistry.NumFiles(), 1)
+	fileRegistry.RangeFiles(func(fd protoreflect.FileDescriptor) bool {
+		s.EqualValues(fd.Package(), "kalisto.tests.examples.service")
+		s.EqualValues(fd.Services().Len(), 1)
+		s.EqualValues(fd.Services().Get(0).Name(), "BookStore")
+		s.EqualValues(fd.Services().Get(0).Methods().Len(), 1)
+		s.EqualValues(fd.Services().Get(0).Methods().Get(0).Name(), "GetBook")
+
+		return true
+	})
+}
+
+func (s *FileCompilerTestSuite) TestMutipleFilesWithNoDeps() {
+
+}
+
+func (s *FileCompilerTestSuite) TestMutipleFilesWithSameLevelDep() {
+
+}
+
+func (s *FileCompilerTestSuite) TestMutipleFilesWithInnerLevelDep() {
+
+}
+
+func (s *FileCompilerTestSuite) TestMutipleFilesWithUpLevelDep() {
+
+}
+
+func (s *FileCompilerTestSuite) TestMutipleFilesWithExternalDep() {
+
+}
+
+func (s *FileCompilerTestSuite) TestMutipleFilesWithCyclicDep() {
+
+}
+
+func (s *FileCompilerTestSuite) TestMutipleDirectories() {
+
+}
+
+func (s *FileCompilerTestSuite) TestMutipleDirectoriesWithInnerLevelDep() {
+
+}
+
+func (s *FileCompilerTestSuite) TestMutipleDirectoriesWithUpLevelDep() {
+
+}
+
+func (s *FileCompilerTestSuite) TestMutipleDirectoriesWithCyclicDep() {
+
+}
+
+func TestFileCompiler(t *testing.T) {
+	suite.Run(t, new(FileCompilerTestSuite))
+}
