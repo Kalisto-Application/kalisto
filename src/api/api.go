3614aadc6782802eec943406aba49317a27db472
Denis <candyboobers@gmail.com>
2023-06-16 17:35:23 +0200

    Add rename and delete workspace api

--- src/api/api.go
+++ src/api/api.go
@@ -7,6 +7,8 @@ import (
 	"kalisto/src/proto/compiler"
 	"kalisto/src/proto/spec"
 	"kalisto/src/workspace"
+
+	"github.com/google/uuid"
 )
 
 type Api struct {
@@ -27,31 +29,40 @@ func New(
 	}
 }
 
-func (a *Api) SpecFromProto(path string) (models.Spec, error) {
+func (a *Api) NewWorkspace(path string) (models.Workspace, error) {
 	protoFiles, err := filesystem.SearchProtoFiles(path)
 	if err != nil {
-		return models.Spec{}, fmt.Errorf("api: failed to search proto files: %w", err)
+		return models.Workspace{}, fmt.Errorf("api: failed to search proto files: %w", err)
 	}
 
 	registry, err := a.compiler.Compile([]string{protoFiles.AbsoluteDirPath}, protoFiles.RelativeProtoPaths)
 	if err != nil {
-		return models.Spec{}, fmt.Errorf("api: failed to compile proto files: %w", err)
+		return models.Workspace{}, fmt.Errorf("api: failed to compile proto files: %w", err)
 	}
 
 	spc, err := a.specFactory.FromRegistry(registry)
 	if err != nil {
-		return models.Spec{}, fmt.Errorf("api: failed to create spec from registry: %w", err)
+		return models.Workspace{}, fmt.Errorf("api: failed to create spec from registry: %w", err)
 	}
 
-	if _, err := a.workspace.Save(models.Workspace{
+	ws, err := a.workspace.Save(models.Workspace{
 		Name:     "New workspace",
 		Spec:     spc,
 		BasePath: path,
-	}); err != nil {
-		return spc, fmt.Errorf("api: failed to save workspace: %w", err)
+	})
+	if err != nil {
+		return ws, fmt.Errorf("api: failed to save workspace: %w", err)
 	}
 
-	return spc, nil
+	return ws, nil
+}
+
+func (s *Api) RenameWorkspace(id uuid.UUID, name string) error {
+	return s.workspace.Rename(id, name)
+}
+
+func (s *Api) DeleteWorkspace(id uuid.UUID) error {
+	return s.workspace.Delete(id)
 }
 
 func (a *Api) SendGrpc(request models.Request) (models.Response, error) {
