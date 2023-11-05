4286e2bbf7b96bec80a000791b6be18c1f0d2036
DenisGrishin <666trall666@gmail.com>
2023-11-05 17:30:32 +0300

    add tree

--- frontend/src/components/MethodCollectionView.tsx
+++ frontend/src/components/MethodCollectionView.tsx
@@ -1,17 +1,15 @@
 import React, { useContext, useEffect, useState } from 'react';
-import {
-  ControlledTreeEnvironment,
-  Tree,
-  TreeItem,
-  TreeItemIndex,
-} from 'react-complex-tree';
+import TreeView, {
+  flattenTree,
+  ITreeViewOnSelectProps,
+} from 'react-accessible-treeview';
 import { models } from '../../wailsjs/go/models';
 import { Context } from '../state';
 
 const findMethod = (
   s: models.Service[] = [],
   serviceName: string,
-  name: string,
+  name: string
 ): models.Method | undefined => {
   for (const service of s) {
     if (service.fullName == serviceName) {
@@ -30,86 +28,48 @@ type Data = {
   parent?: string;
 };
 
-export const MethodCollection: React.FC = () => {
+export const MethodCollection: React.FC<ITreeViewOnSelectProps> = () => {
   const ctx = useContext(Context);
 
   const services = ctx.state.activeWorkspace?.spec.services;
   const selectedItem = ctx.state.activeMethod?.fullName;
 
   const serviceNames = services?.map((it) => it.fullName) || [];
-  const [expandedItems, setExpandedItems] =
-    useState<TreeItemIndex[]>(serviceNames);
+  console.log('services', services);
 
-  useEffect(() => {
-    setExpandedItems(serviceNames);
-  }, [services]);
-
-  const itemsData: Record<TreeItemIndex, TreeItem<Data>> = {
-    root: {
-      index: 'root',
-      isFolder: true,
-      data: { display: 'Root item', isMethod: false },
-      children: serviceNames,
-    },
+  const newFolder = {
+    name: '',
+    children:
+      services?.map((it) => {
+        return {
+          name: it.displayName,
+          children:
+            it.methods.map((met) => {
+              return {
+                name: met.name,
+              };
+            }) || [],
+        };
+      }) || [],
   };
-  services?.forEach((it) => {
-    itemsData[it.fullName] = {
-      index: it.fullName,
-      data: { display: it.displayName, isMethod: false },
-      isFolder: true,
-      children: it.methods.map((met) => met.fullName),
-    };
-    it.methods.forEach((met) => {
-      itemsData[met.fullName] = {
-        index: met.fullName,
-        data: { display: met.name, isMethod: true, parent: it.fullName },
-      };
-    });
-  });
+
+  console.log('new', newFolder);
+
+  const data = flattenTree(newFolder);
 
   return (
     <div>
-      <ControlledTreeEnvironment
-        items={itemsData}
-        getItemTitle={(item) => item.data.display}
-        viewState={{
-          '1': {
-            expandedItems: expandedItems,
-            selectedItems: selectedItem ? [selectedItem] : undefined,
-          },
-        }}
-        onExpandItem={(item) => {
-          if (item.data.isMethod) {
-            const method = findMethod(
-              services,
-              item.data.parent || '',
-              item.index as string,
-            );
-
-            ctx.dispatch({ type: 'activeMethod', activeMethod: method! });
-          }
-          setExpandedItems([...expandedItems, item.index]);
-        }}
-        onCollapseItem={(item) => {
-          setExpandedItems(
-            expandedItems.filter(
-              (expandedItemIndex) => expandedItemIndex !== item.index,
-            ),
-          );
-        }}
-        onFocusItem={(item) => {
-          if (item.data.isMethod) {
-            const method = findMethod(
-              services,
-              item.data.parent || '',
-              item.index as string,
-            );
-            ctx.dispatch({ type: 'activeMethod', activeMethod: method! });
-          }
-        }}
-      >
-        <Tree treeId="1" rootItem="root" treeLabel="methods collection" />
-      </ControlledTreeEnvironment>
+      <div className="directory">
+        <TreeView
+          data={data}
+          aria-label="directory tree"
+          nodeRenderer={({ element, getNodeProps, level }) => (
+            <div {...getNodeProps()} style={{ paddingLeft: 20 * (level - 1) }}>
+              {element.name}
+            </div>
+          )}
+        />
+      </div>
     </div>
   );
 };
